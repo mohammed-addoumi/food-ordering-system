76e1c094c3c7275beed72e1bed32f0583b62d411
addoumi mohammed <addoumi.mohammed@gmail.com>
2022-07-25 23:54:05 +0100

    kafka avro schemas

--- infrastructure/kafka/kafka-config-data/src/main/java/com/food/ordering/system/infrastructure/kafka/config/KafkaConfigData.java
+++ infrastructure/kafka/kafka-config-data/src/main/java/com/food/ordering/system/infrastructure/kafka/config/KafkaConfigData.java
@@ -1,9 +1,16 @@
 package com.food.ordering.system.infrastructure.kafka.config;
 
+import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
+@Data
 @Configuration
-@ConfigurationProperties(prefix = "")
+@ConfigurationProperties(prefix = "kafka-config")
 public class KafkaConfigData {
+    private String bootstrapServers;
+    private String schemaRegistryUrlKey;
+    private String schemaRegistryUrl;
+    private Integer numOfPartitions;
+    private Short replicationFactor;
 }
